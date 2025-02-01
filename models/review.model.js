bab67e285da93272c03b9a9232eb0782867bd2d3
mikugh <mikael.granqvist@abo.fi>
2025-02-01 15:41:53 +0200

    made bugfixes for score_sum and num_of_reviews

--- models/review.model.js
+++ models/review.model.js
@@ -4,7 +4,7 @@ const ReviewSchema = mongoose.Schema({
     id: mongoose.ObjectId,
     author: {type: String, required: true, maxLength: 64},
     review: {type: String, required: true, maxLength: 4096},
-    rating: {type: Number, required: true},
+    score: {type: Number, required: true},
     market: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Market'
--- routes/review.route.js
+++ routes/review.route.js
@@ -27,12 +27,13 @@ router.get('/getByMarket/:marketID', async (req, res) => {
 router.post('/', async (req,res) => {
     try {
         // get the market id from the json
-        const { marketID } = req.body.market;
+        const marketID = req.body.market;
         // get the market with that id
         const market = await Market.findById(marketID);
         // update the market object
         market.score_sum = market.score_sum + req.body.score;
         market.num_of_reviews = market.num_of_reviews + 1;
+        const updated_market = await Market.findByIdAndUpdate(marketID, market);
         // save the review object
         const review = await Review.create(req.body);
         res.status(200).json(review);
@@ -55,15 +56,17 @@ router.put('/:id', async (req,res) => {
 router.delete('/:id', async (req,res) => {
     try {
         const { id } = req.params;
-        const review = await Review.findByIdAndDelete(id);
-         // get the market id from the json
-         const { marketID } = review.market;
-         // get the market with that id
-         const market = await Market.findById(marketID);
-         // update the market object
-         market.score_sum = market.score_sum - review.score;
-         market.num_of_reviews = market.num_of_reviews - 1;
-         // save the review object
+        const review = await Review.findById(id);
+        const deleted_market_review = await Review.findByIdAndDelete(id);
+        // get the market id from the json
+        const marketID = review.market;
+        // get the market with that id
+        const market = await Market.findById(marketID);
+        // update the market object
+        market.score_sum = market.score_sum - review.score;
+        market.num_of_reviews = market.num_of_reviews - 1;
+        const updated_market = await Market.findByIdAndUpdate(marketID, market);
+        // save the review object
         if (!review) { return res.status(404).json({message: "Review not found"}); }
         res.status(200).json({message: "Review deleted succesfully"});
     } catch (error) {
